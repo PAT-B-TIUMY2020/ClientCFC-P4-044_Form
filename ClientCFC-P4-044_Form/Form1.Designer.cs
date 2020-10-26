52b78c83368d86adfd41a5d968b03493ea32aac8
Fakhrizar Rifqi Fuad <fakhrizar.rifqi.ft18@mail.umy.ac.id>
2020-10-26 16:02:29 +0700

    First Commit

--- ClientCFC-P4-044_Form/Form1.Designer.cs
+++ ClientCFC-P4-044_Form/Form1.Designer.cs
@@ -28,13 +28,247 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtHasilPembagian = new System.Windows.Forms.TextBox();
+            this.txtPembagian2 = new System.Windows.Forms.TextBox();
+            this.txtPembagian1 = new System.Windows.Forms.TextBox();
+            this.txtHasilPerkalian = new System.Windows.Forms.TextBox();
+            this.txtPerkalian2 = new System.Windows.Forms.TextBox();
+            this.txtPerkalian1 = new System.Windows.Forms.TextBox();
+            this.txtHasilPengurangan = new System.Windows.Forms.TextBox();
+            this.txtPengurangan2 = new System.Windows.Forms.TextBox();
+            this.txtPengurangan1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHasilPenjumlahan = new System.Windows.Forms.TextBox();
+            this.txtPenjumlahan2 = new System.Windows.Forms.TextBox();
+            this.txtPenjumlahan1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(168, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 16);
+            this.label8.TabIndex = 39;
+            this.label8.Text = "/";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(168, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 16);
+            this.label7.TabIndex = 38;
+            this.label7.Text = "x";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(169, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 16);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "-";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(317, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 16);
+            this.label5.TabIndex = 36;
+            this.label5.Text = "=";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(317, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 16);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(317, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "=";
+            // 
+            // txtHasilPembagian
+            // 
+            this.txtHasilPembagian.Location = new System.Drawing.Point(349, 213);
+            this.txtHasilPembagian.Name = "txtHasilPembagian";
+            this.txtHasilPembagian.Size = new System.Drawing.Size(100, 20);
+            this.txtHasilPembagian.TabIndex = 33;
+            // 
+            // txtPembagian2
+            // 
+            this.txtPembagian2.Location = new System.Drawing.Point(199, 213);
+            this.txtPembagian2.Name = "txtPembagian2";
+            this.txtPembagian2.Size = new System.Drawing.Size(100, 20);
+            this.txtPembagian2.TabIndex = 32;
+            // 
+            // txtPembagian1
+            // 
+            this.txtPembagian1.Location = new System.Drawing.Point(49, 214);
+            this.txtPembagian1.Name = "txtPembagian1";
+            this.txtPembagian1.Size = new System.Drawing.Size(100, 20);
+            this.txtPembagian1.TabIndex = 31;
+            // 
+            // txtHasilPerkalian
+            // 
+            this.txtHasilPerkalian.Location = new System.Drawing.Point(349, 168);
+            this.txtHasilPerkalian.Name = "txtHasilPerkalian";
+            this.txtHasilPerkalian.Size = new System.Drawing.Size(100, 20);
+            this.txtHasilPerkalian.TabIndex = 30;
+            // 
+            // txtPerkalian2
+            // 
+            this.txtPerkalian2.Location = new System.Drawing.Point(199, 168);
+            this.txtPerkalian2.Name = "txtPerkalian2";
+            this.txtPerkalian2.Size = new System.Drawing.Size(100, 20);
+            this.txtPerkalian2.TabIndex = 29;
+            // 
+            // txtPerkalian1
+            // 
+            this.txtPerkalian1.Location = new System.Drawing.Point(49, 169);
+            this.txtPerkalian1.Name = "txtPerkalian1";
+            this.txtPerkalian1.Size = new System.Drawing.Size(100, 20);
+            this.txtPerkalian1.TabIndex = 28;
+            // 
+            // txtHasilPengurangan
+            // 
+            this.txtHasilPengurangan.Location = new System.Drawing.Point(349, 119);
+            this.txtHasilPengurangan.Name = "txtHasilPengurangan";
+            this.txtHasilPengurangan.Size = new System.Drawing.Size(100, 20);
+            this.txtHasilPengurangan.TabIndex = 27;
+            // 
+            // txtPengurangan2
+            // 
+            this.txtPengurangan2.Location = new System.Drawing.Point(199, 120);
+            this.txtPengurangan2.Name = "txtPengurangan2";
+            this.txtPengurangan2.Size = new System.Drawing.Size(100, 20);
+            this.txtPengurangan2.TabIndex = 26;
+            // 
+            // txtPengurangan1
+            // 
+            this.txtPengurangan1.Location = new System.Drawing.Point(49, 121);
+            this.txtPengurangan1.Name = "txtPengurangan1";
+            this.txtPengurangan1.Size = new System.Drawing.Size(100, 20);
+            this.txtPengurangan1.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(317, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "=";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 16);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "+";
+            // 
+            // txtHasilPenjumlahan
+            // 
+            this.txtHasilPenjumlahan.Location = new System.Drawing.Point(349, 71);
+            this.txtHasilPenjumlahan.Name = "txtHasilPenjumlahan";
+            this.txtHasilPenjumlahan.Size = new System.Drawing.Size(100, 20);
+            this.txtHasilPenjumlahan.TabIndex = 22;
+            // 
+            // txtPenjumlahan2
+            // 
+            this.txtPenjumlahan2.Location = new System.Drawing.Point(199, 71);
+            this.txtPenjumlahan2.Name = "txtPenjumlahan2";
+            this.txtPenjumlahan2.Size = new System.Drawing.Size(100, 20);
+            this.txtPenjumlahan2.TabIndex = 21;
+            // 
+            // txtPenjumlahan1
+            // 
+            this.txtPenjumlahan1.Location = new System.Drawing.Point(49, 71);
+            this.txtPenjumlahan1.Name = "txtPenjumlahan1";
+            this.txtPenjumlahan1.Size = new System.Drawing.Size(100, 20);
+            this.txtPenjumlahan1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(527, 289);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHasilPembagian);
+            this.Controls.Add(this.txtPembagian2);
+            this.Controls.Add(this.txtPembagian1);
+            this.Controls.Add(this.txtHasilPerkalian);
+            this.Controls.Add(this.txtPerkalian2);
+            this.Controls.Add(this.txtPerkalian1);
+            this.Controls.Add(this.txtHasilPengurangan);
+            this.Controls.Add(this.txtPengurangan2);
+            this.Controls.Add(this.txtPengurangan1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHasilPenjumlahan);
+            this.Controls.Add(this.txtPenjumlahan2);
+            this.Controls.Add(this.txtPenjumlahan1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtHasilPembagian;
+        private System.Windows.Forms.TextBox txtPembagian2;
+        private System.Windows.Forms.TextBox txtPembagian1;
+        private System.Windows.Forms.TextBox txtHasilPerkalian;
+        private System.Windows.Forms.TextBox txtPerkalian2;
+        private System.Windows.Forms.TextBox txtPerkalian1;
+        private System.Windows.Forms.TextBox txtHasilPengurangan;
+        private System.Windows.Forms.TextBox txtPengurangan2;
+        private System.Windows.Forms.TextBox txtPengurangan1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHasilPenjumlahan;
+        private System.Windows.Forms.TextBox txtPenjumlahan2;
+        private System.Windows.Forms.TextBox txtPenjumlahan1;
     }
 }
 
--- ClientCFC-P4-044_Form/Form1.cs
+++ ClientCFC-P4-044_Form/Form1.cs
@@ -16,5 +16,44 @@ namespace ClientCFC_P4_044_Form
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPenjumlahan1.Enabled = false;
+            txtPenjumlahan2.Enabled = false;
+            txtPengurangan1.Enabled = false;
+            txtPengurangan2.Enabled = false;
+            txtPerkalian1.Enabled = false;
+            txtPerkalian2.Enabled = false;
+            txtPembagian1.Enabled = false;
+            txtPembagian2.Enabled = false;
+            txtHasilPenjumlahan.Enabled = false;
+            txtHasilPengurangan.Enabled = false;
+            txtHasilPerkalian.Enabled = false;
+            txtHasilPembagian.Enabled = false;
+
+
+            int hasil;
+
+            txtPenjumlahan1.Text = "1";
+            txtPenjumlahan2.Text = "2";
+            hasil = Convert.ToInt32(txtPenjumlahan1.Text) + Convert.ToInt32(txtPenjumlahan2.Text);
+            txtHasilPenjumlahan.Text = hasil.ToString();
+
+            txtPengurangan1.Text = "3";
+            txtPengurangan2.Text = "2";
+            hasil = Convert.ToInt32(txtPengurangan1.Text) - Convert.ToInt32(txtPengurangan2.Text);
+            txtHasilPengurangan.Text = hasil.ToString();
+
+            txtPerkalian1.Text = "2";
+            txtPerkalian2.Text = "2";
+            hasil = Convert.ToInt32(txtPerkalian1.Text) * Convert.ToInt32(txtPerkalian2.Text);
+            txtHasilPerkalian.Text = hasil.ToString();
+
+            txtPembagian1.Text = "2";
+            txtPembagian2.Text = "2";
+            hasil = Convert.ToInt32(txtPembagian1.Text) / Convert.ToInt32(txtPembagian2.Text);
+            txtHasilPembagian.Text = hasil.ToString();
+        }
     }
 }
